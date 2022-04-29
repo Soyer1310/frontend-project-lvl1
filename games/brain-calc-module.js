b1048437e781f64fd066c77ac05822328e21c998
Soyer1310 <soyer131313@gmail.com>
2022-04-29 20:05:39 +0300

    add gamesdir

---
+++ games/brain-calc-module.js
@@ -0,0 +1,27 @@
+import randomNumber from '../src/random-number.js';
+
+export const task = 'What is the result of the expression?';
+
+export const getQuestion = () => {
+  const operators = ['+', '-', '*'];
+  const operator = operators[randomNumber(0, 2)];
+  const question = `${randomNumber(1, 100)} ${operator} ${randomNumber(1, 100)}`;
+  return question;
+};
+
+export const getAnswer = (question) => {
+  const questionArr = question.split(' ');
+  const num1 = +questionArr[0];
+  const num2 = +questionArr[2];
+  const operator = questionArr[1];
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return null;
+  }
+};
