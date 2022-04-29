9ba673606b9b0ba47cbde7277d35fec492dc8bcc
Soyer1310 <soyer131313@gmail.com>
2022-04-29 20:08:47 +0300

    add index.js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import readlineSync from 'readline-sync';
+import askName from './cli.js';
+
+export default (task, getQuestion, getAnswer) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = askName();
+  console.log(task);
+  for (let i = 0; i < 3; i += 1) {
+    const taskQuestion = getQuestion();
+    const correctAnswer = getAnswer(taskQuestion);
+    const userAnswer = readlineSync.question(`Question: ${taskQuestion} `);
+    if (+userAnswer === correctAnswer) {
+      console.log(`Your answer: ${userAnswer}`);
+      console.log('Correct!');
+    } else {
+      console.log(`Your answer: ${userAnswer}`);
+      console.log(`'${userAnswer}' is wrong answer. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
