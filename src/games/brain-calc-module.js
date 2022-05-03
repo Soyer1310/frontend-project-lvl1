eb926ce0d95d4465e55808c174b30686523b13b6
Soyer1310 <soyer131313@gmail.com>
2022-05-03 23:58:04 +0300

    fix adding in brain-calc-module.js

--- src/games/brain-calc-module.js
+++ src/games/brain-calc-module.js
@@ -7,7 +7,7 @@ const calc = (num1, num2, operator) => {
   let result = null;
   switch (operator) {
     case '+':
-      result = num1 + num2;
+      result = +num1 + +num2;
       break;
     case '-':
       result = num1 - num2;
