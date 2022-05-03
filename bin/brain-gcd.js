7d5608346a0934541acda08bd3649e03a0ec1e2b
Soyer1310 <soyer131313@gmail.com>
2022-05-03 20:39:21 +0300

    change brain-gcd.js imports

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import index from '../src/index.js';
-import { task, getQuestion, getAnswer } from '../games/brain-gcd-module.js';
+import runGCDGame from '../src/games/brain-gcd-module.js';
 
-index(task, getQuestion, getAnswer);
+runGCDGame();
