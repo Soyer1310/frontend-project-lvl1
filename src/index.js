486cf082e9a23bd094c692cc7ac36d867bed360f
Soyer1310 <soyer131313@gmail.com>
2022-05-03 19:46:41 +0300

    evolve index.js logic

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import readlineSync from 'readline-sync';
 
-export default (task, getQuestion, getAnswer) => {
+export default (game, task) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(task);
   const roundsCount = 3;
   for (let i = 0; i < roundsCount; i += 1) {
-    const taskQuestion = getQuestion();
-    const correctAnswer = getAnswer(taskQuestion);
-    const userAnswer = readlineSync.question(`Question: ${taskQuestion} `);
-    if (userAnswer === correctAnswer) {
+    const round = game();
+    const question = round[0];
+    const answer = round[1];
+    const userAnswer = readlineSync.question(`Question: ${question} `);
+    if (userAnswer === answer) {
       console.log(`Your answer: ${userAnswer}`);
       console.log('Correct!');
     } else {
       console.log(`Your answer: ${userAnswer}`);
-      console.log(`'${userAnswer}' is wrong answer. Correct answer was '${correctAnswer}'.`);
+      console.log(`'${userAnswer}' is wrong answer. Correct answer was '${answer}'.`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
