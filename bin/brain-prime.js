b5a65634839de83fbf641d409d4b1ed966746550
Soyer1310 <soyer131313@gmail.com>
2022-05-03 22:09:14 +0300

    change bin/brain-prime.js exports

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import index from '../src/index.js';
-import { task, getQuestion, getAnswer } from '../games/brain-prime-module.js';
+import runPrimeGame from '../src/games/brain-prime-module.js';
 
-index(task, getQuestion, getAnswer);
+runPrimeGame();
