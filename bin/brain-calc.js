fee82c7ec4a7248940cbab3261dbac4e5759b297
Soyer1310 <soyer131313@gmail.com>
2022-05-03 20:32:59 +0300

    change imports bin/brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import index from '../src/index.js';
-import { task, getQuestion, getAnswer } from '../games/brain-calc-module.js';
+import runCalcGame from '../src/games/brain-calc-module.js';
 
-index(task, getQuestion, getAnswer);
+runCalcGame();
